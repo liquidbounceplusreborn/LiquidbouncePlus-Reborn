4df4a9c5ff7dbec4a422b5f9e5e33db68f9dd0b5
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2022-10-15 09:36:01 +0800

    Update Rotations.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Rotations.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Rotations.kt
@@ -1,68 +1,13 @@
-/*
- * LiquidBounce+ Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/WYSI-Foundation/LiquidBouncePlus/
- */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
-import net.ccbluex.liquidbounce.LiquidBounce
-import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.features.module.modules.combat.BowAimbot
-import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
-import net.ccbluex.liquidbounce.features.module.modules.exploit.Disabler
-import net.ccbluex.liquidbounce.features.module.modules.movement.Fly
-import net.ccbluex.liquidbounce.features.module.modules.movement.Sprint
-import net.ccbluex.liquidbounce.features.module.modules.world.*
-import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.value.BoolValue
-import net.ccbluex.liquidbounce.value.ListValue
-import net.minecraft.network.play.client.C03PacketPlayer
 
-@ModuleInfo(name = "Rotations", description = "Allows you to see server-sided head and body rotations.", category = ModuleCategory.RENDER)
+@ModuleInfo(name = "Rotations", category = ModuleCategory.RENDER, forceNoSound = true, onlyEnable = true, array = false)
 class Rotations : Module() {
 
-    val modeValue = ListValue("Mode", arrayOf("Head", "Body"), "Body")
-
-    private var playerYaw: Float? = null
-
-    @EventTarget
-    fun onRender3D(event: Render3DEvent) {
-        if (modeValue.get().equals("head", true) && RotationUtils.serverRotation != null)
-            mc.thePlayer.rotationYawHead = RotationUtils.serverRotation.yaw
-    }
-    @EventTarget
-    fun onPacket(event: PacketEvent) {
-        if (modeValue.get().equals("head", true) || !shouldRotate() || mc.thePlayer == null) {
-            playerYaw = null
-            return
-        }
-
-        val packet = event.packet
-        if (packet is C03PacketPlayer && packet.rotating) {
-            playerYaw = packet.yaw
-            mc.thePlayer.renderYawOffset = packet.getYaw()
-            mc.thePlayer.rotationYawHead = packet.getYaw()
-        } else {
-            if (playerYaw != null)
-                mc.thePlayer.renderYawOffset = this.playerYaw!!
-            mc.thePlayer.rotationYawHead = mc.thePlayer.renderYawOffset
-        }
-    }
-
-    private fun getState(module: Class<out Module>) = LiquidBounce.moduleManager[module]!!.state
-
-    fun shouldRotate(): Boolean {
-        val killAura = LiquidBounce.moduleManager.getModule(KillAura::class.java) as KillAura
-        val disabler = LiquidBounce.moduleManager.getModule(Disabler::class.java) as Disabler
-        val sprint = LiquidBounce.moduleManager.getModule(Sprint::class.java) as Sprint
-        return getState(Scaffold::class.java) ||
-                (getState(Sprint::class.java) && sprint.allDirectionsValue.get() && sprint.moveDirPatchValue.get()) ||
-                (getState(KillAura::class.java) && killAura.target != null) ||
-                (getState(Disabler::class.java) && disabler.canRenderInto3D) ||
-                getState(BowAimbot::class.java) || getState(Fucker::class.java) ||
-                getState(ChestAura::class.java) || getState(Fly::class.java)
-    }
+    val headValue = BoolValue("Head", true)
+    val bodyValue = BoolValue("Body", true)
 }
