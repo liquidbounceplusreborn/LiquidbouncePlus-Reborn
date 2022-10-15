2384fedcb471847a9a159f8b72198cf9e7947a92
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2022-10-15 09:37:17 +0800

    Update Rotations.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Rotations.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Rotations.kt
@@ -5,7 +5,7 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.value.BoolValue
 
-@ModuleInfo(name = "Rotations", category = ModuleCategory.RENDER, forceNoSound = true, onlyEnable = true, array = false)
+@ModuleInfo(name = "Rotations", description = "Allows you to see server-sided head and body rotations.", category = ModuleCategory.RENDER, forceNoSound = true, onlyEnable = true, array = false)
 class Rotations : Module() {
 
     val headValue = BoolValue("Head", true)
