2ddf847fcdc17223667a4fef96f8de4a932528b7
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2022-10-30 17:53:49 +0800

    lmfao

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
@@ -1,9 +1,4 @@
-/*
- * FDPClient Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- *
- * https://github.com/Project-EZ4H/FDPClient/
- */
+
 package net.ccbluex.liquidbounce.ui.client.hud.element.elements
 
 import net.ccbluex.liquidbounce.LiquidBounce
