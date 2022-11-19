8e51bb165899c42d944b8cb05606d029ec471bab
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2022-11-19 20:35:44 +0800

    Update MixinLayerHeldItem.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinLayerHeldItem.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinLayerHeldItem.java
@@ -55,8 +55,8 @@ public class MixinLayerHeldItem {
             final UUID uuid = entitylivingbaseIn.getUniqueID();
             final EntityPlayer entityplayer = Minecraft.getMinecraft().theWorld.getPlayerEntityByUUID(uuid);
 
-            if(entityplayer != null && entityplayer.isBlocking()) {
-                if(entitylivingbaseIn.isSneaking() || Animations.fakeBlock.get()){
+            if (entityplayer != null && entityplayer.isBlocking() || Animations.fakeBlock.get()) {
+                if (entitylivingbaseIn.isSneaking()) {
                     ((ModelBiped) this.livingEntityRenderer.getMainModel()).postRenderArm(0.0325F);
                     GlStateManager.translate(-0.58F, 0.3F, -0.2F);
                     GlStateManager.rotate(-24390.0F, 137290.0F, -2009900.0F, -2054900.0F);
