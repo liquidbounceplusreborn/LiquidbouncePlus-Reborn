4336844fbb5b25585bf1352475307e66ef30a7ba
wxdbie <101655364+wxdbie@users.noreply.github.com>
2022-12-02 20:40:50 +0500

    WorldColor in Camera

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinEntityRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinEntityRenderer.java
@@ -6,46 +6,83 @@
 package net.ccbluex.liquidbounce.injection.forge.mixins.render;
 
 import com.google.common.base.Predicates;
+import com.sun.javafx.geom.Vec3d;
 import net.ccbluex.liquidbounce.LiquidBounce;
 import net.ccbluex.liquidbounce.event.Render3DEvent;
 import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura;
 import net.ccbluex.liquidbounce.features.module.modules.player.Reach;
 import net.ccbluex.liquidbounce.features.module.modules.render.Camera;
 import net.ccbluex.liquidbounce.features.module.modules.render.NoHurtCam;
+import net.ccbluex.liquidbounce.features.module.modules.render.Rotations;
 import net.ccbluex.liquidbounce.features.module.modules.render.TargetMark;
 import net.ccbluex.liquidbounce.features.module.modules.render.Tracers;
+import net.ccbluex.liquidbounce.features.module.modules.world.Ambience;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItemFrame;
 import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.*;
+import net.minecraft.world.World;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.awt.*;
 import java.util.List;
 
 @Mixin(EntityRenderer.class)
 public abstract class MixinEntityRenderer {
 
+    protected MixinEntityRenderer(int[] lightmapColors, DynamicTexture lightmapTexture, float torchFlickerX, float bossColorModifier, float bossColorModifierPrev, Minecraft mc, float thirdPersonDistanceTemp, float thirdPersonDistance) {
+        this.lightmapColors = lightmapColors;
+        this.lightmapTexture = lightmapTexture;
+        this.torchFlickerX = torchFlickerX;
+        this.bossColorModifier = bossColorModifier;
+        this.bossColorModifierPrev = bossColorModifierPrev;
+        this.mc = mc;
+        this.thirdPersonDistanceTemp = thirdPersonDistanceTemp;
+        this.thirdPersonDistance = thirdPersonDistance;
+    }
+
     @Shadow
     public abstract void loadShader(ResourceLocation resourceLocationIn);
+
+    @Shadow
+    private final int[] lightmapColors;
+
+    @Shadow
+    private final DynamicTexture lightmapTexture;
 
     @Shadow
     public abstract void setupCameraTransform(float partialTicks, int pass);
 
     @Shadow
     private Entity pointedEntity;
+
+    @Shadow
+    private float torchFlickerX;
+
+    @Shadow
+    private float bossColorModifier;
+
+    @Shadow
+    private float bossColorModifierPrev;
+
+    @Shadow
+    private boolean lightmapUpdateNeeded;
 
     @Shadow
     private Minecraft mc;
@@ -101,6 +138,114 @@ public abstract class MixinEntityRenderer {
     private void injectHurtCameraEffect(CallbackInfo callbackInfo) {
         if (LiquidBounce.moduleManager.getModule(NoHurtCam.class).getState())
             callbackInfo.cancel();
+    }
+
+    @Overwrite
+    private void updateLightmap(float f2) {
+        Camera camera = LiquidBounce.moduleManager.getModule(Camera.class);
+        if (this.lightmapUpdateNeeded) {
+            this.mc.mcProfiler.startSection("lightTex");
+            World world = this.mc.theWorld;
+            if (world != null) {
+                float f3 = world.getSunBrightness(1.0f);
+                float f4 = f3 * 0.95f + 0.05f;
+                for (int i2 = 0; i2 < 256; ++i2) {
+                    float f5;
+                    float f6;
+                    float f7 = world.provider.getLightBrightnessTable()[i2 / 16] * f4;
+                    float f8 = world.provider.getLightBrightnessTable()[i2 % 16] * (this.torchFlickerX * 0.1f + 1.5f);
+                    if (world.getLastLightningBolt() > 0) {
+                        f7 = world.provider.getLightBrightnessTable()[i2 / 16];
+                    }
+                    float f9 = f7 * (f3 * 0.65f + 0.35f);
+                    float f10 = f7 * (f3 * 0.65f + 0.35f);
+                    float f11 = f8 * ((f8 * 0.6f + 0.4f) * 0.6f + 0.4f);
+                    float f12 = f8 * (f8 * f8 * 0.6f + 0.4f);
+                    float f13 = f9 + f8;
+                    float f14 = f10 + f11;
+                    float f15 = f7 + f12;
+                    f13 = f13 * 0.96f + 0.03f;
+                    f14 = f14 * 0.96f + 0.03f;
+                    f15 = f15 * 0.96f + 0.03f;
+                    if (this.bossColorModifier > 0.0f) {
+                        float f16 = this.bossColorModifierPrev + (this.bossColorModifier - this.bossColorModifierPrev) * f2;
+                        f13 = f13 * (1.0f - f16) + f13 * 0.7f * f16;
+                        f14 = f14 * (1.0f - f16) + f14 * 0.6f * f16;
+                        f15 = f15 * (1.0f - f16) + f15 * 0.6f * f16;
+                    }
+                    if (world.provider.getDimensionId() == 1) {
+                        f13 = 0.22f + f8 * 0.75f;
+                        f14 = 0.28f + f11 * 0.75f;
+                        f15 = 0.25f + f12 * 0.75f;
+                    }
+                    if (this.mc.thePlayer.isPotionActive(Potion.nightVision)) {
+                        f6 = this.getNightVisionBrightness((EntityLivingBase)this.mc.thePlayer, f2);
+                        f5 = 1.0f / f13;
+                        if (f5 > 1.0f / f14) {
+                            f5 = 1.0f / f14;
+                        }
+                        if (f5 > 1.0f / f15) {
+                            f5 = 1.0f / f15;
+                        }
+                        f13 = f13 * (1.0f - f6) + f13 * f5 * f6;
+                        f14 = f14 * (1.0f - f6) + f14 * f5 * f6;
+                        f15 = f15 * (1.0f - f6) + f15 * f5 * f6;
+                    }
+                    if (f13 > 1.0f) {
+                        f13 = 1.0f;
+                    }
+                    if (f14 > 1.0f) {
+                        f14 = 1.0f;
+                    }
+                    if (f15 > 1.0f) {
+                        f15 = 1.0f;
+                    }
+                    f6 = this.mc.gameSettings.gammaSetting;
+                    f5 = 1.0f - f13;
+                    float f17 = 1.0f - f14;
+                    float f18 = 1.0f - f15;
+                    f5 = 1.0f - f5 * f5 * f5 * f5;
+                    f17 = 1.0f - f17 * f17 * f17 * f17;
+                    f18 = 1.0f - f18 * f18 * f18 * f18;
+                    f13 = f13 * (1.0f - f6) + f5 * f6;
+                    f14 = f14 * (1.0f - f6) + f17 * f6;
+                    f15 = f15 * (1.0f - f6) + f18 * f6;
+                    f13 = f13 * 0.96f + 0.03f;
+                    f14 = f14 * 0.96f + 0.03f;
+                    f15 = f15 * 0.96f + 0.03f;
+                    if (f13 > 1.0f) {
+                        f13 = 1.0f;
+                    }
+                    if (f14 > 1.0f) {
+                        f14 = 1.0f;
+                    }
+                    if (f15 > 1.0f) {
+                        f15 = 1.0f;
+                    }
+                    if (f13 < 0.0f) {
+                        f13 = 0.0f;
+                    }
+                    if (f14 < 0.0f) {
+                        f14 = 0.0f;
+                    }
+                    if (f15 < 0.0f) {
+                        f15 = 0.0f;
+                    }
+                    int n2 = (int)(f13 * 255.0f);
+                    int n3 = (int)(f14 * 255.0f);
+                    int n4 = (int)(f15 * 255.0f);
+                    this.lightmapColors[i2] = camera.getState() && camera.getWordlColorValue().get() ? new Color(camera.getWordlColorrValue().get(),camera.getWordlColorgValue().get(),camera.getWordlColorbValue().get()).getRGB() : 0xFF000000 | n2 << 16 | n3 << 8 | n4;
+                }
+                this.lightmapTexture.updateDynamicTexture();
+                this.lightmapUpdateNeeded = false;
+                this.mc.mcProfiler.endSection();
+            }
+        }
+    }
+
+    private float getNightVisionBrightness(EntityLivingBase p_getNightVisionBrightness_1_, float p_getNightVisionBrightness_2_) {
+        int i = p_getNightVisionBrightness_1_.getActivePotionEffect(Potion.nightVision).getDuration();
+        return i > 200 ? 1.0F : 0.7F + MathHelper.sin(((float)i - p_getNightVisionBrightness_2_) * 3.1415927F * 0.2F) * 0.3F;
     }
 
     @Inject(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Vec3;distanceTo(Lnet/minecraft/util/Vec3;)D"), cancellable = true)
