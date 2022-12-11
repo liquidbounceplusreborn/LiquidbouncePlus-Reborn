479b1c2057b0d3331a136f073417812a74a32300
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2022-12-11 17:19:39 +0800

    Update MixinLayerHeldItem.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinLayerHeldItem.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinLayerHeldItem.java
@@ -5,9 +5,6 @@
  */
 package net.ccbluex.liquidbounce.injection.forge.mixins.render;
 
-import net.ccbluex.liquidbounce.LiquidBounce;
-import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura;
-import net.ccbluex.liquidbounce.features.module.modules.render.Animations;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBiped;
@@ -21,7 +18,6 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemSword;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -42,8 +38,6 @@ public class MixinLayerHeldItem {
     @Overwrite
     public void doRenderLayer(EntityLivingBase entitylivingbaseIn, float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_, float p_177141_7_, float scale) {
         ItemStack itemstack = entitylivingbaseIn.getHeldItem();
-        final Animations animations = LiquidBounce.moduleManager.getModule(Animations.class);
-        final KillAura killaura = LiquidBounce.moduleManager.getModule(KillAura.class);
 
         if(itemstack != null) {
             GlStateManager.pushMatrix();
@@ -55,12 +49,11 @@ public class MixinLayerHeldItem {
                 GlStateManager.scale(f, f, f);
             }
 
-            Item item = itemstack.getItem();
             final UUID uuid = entitylivingbaseIn.getUniqueID();
             final EntityPlayer entityplayer = Minecraft.getMinecraft().theWorld.getPlayerEntityByUUID(uuid);
 
-            if (entityplayer != null && entityplayer.isBlocking() || killaura.getTarget() != null && Animations.fakeBlock.get() && item instanceof ItemSword) {
-                if (entitylivingbaseIn.isSneaking()) {
+            if(entityplayer != null && entityplayer.isBlocking()) {
+                if(entitylivingbaseIn.isSneaking()) {
                     ((ModelBiped) this.livingEntityRenderer.getMainModel()).postRenderArm(0.0325F);
                     GlStateManager.translate(-0.58F, 0.3F, -0.2F);
                     GlStateManager.rotate(-24390.0F, 137290.0F, -2009900.0F, -2054900.0F);
@@ -79,6 +72,7 @@ public class MixinLayerHeldItem {
                 itemstack = new ItemStack(Items.fishing_rod, 0);
             }
 
+            Item item = itemstack.getItem();
             Minecraft minecraft = Minecraft.getMinecraft();
 
             if(item instanceof ItemBlock && Block.getBlockFromItem(item).getRenderType() == 2) {
