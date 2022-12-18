6dd5f7eb8e59dc993f89c2c1372d5a35e5989f7e
wxdbie <101655364+wxdbie@users.noreply.github.com>
2022-12-19 03:55:33 +0500

    Add CustomFog in Camera

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Camera.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Camera.kt
@@ -5,22 +5,28 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
+import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.value.BoolValue
-import net.ccbluex.liquidbounce.value.IntegerValue
-import net.ccbluex.liquidbounce.value.ListValue
+import net.ccbluex.liquidbounce.value.*
+import java.awt.Color
 
 @ModuleInfo(name = "Camera", description = "Allows you to see through walls in third person view.", category = ModuleCategory.RENDER)
 class Camera : Module(){
     val cameraClipValue = BoolValue("CameraClip", true)
     val antiBlindValue = BoolValue("AntiBlind", true)
     val wordlColorValue = BoolValue("WorldColor", true)
-    //
+    val fogColorValue = BoolValue("FogColor", true)
+    //WorldColor
     val wordlColorrValue = IntegerValue("Red", 255, 0, 255) { wordlColorValue.get() }
     val wordlColorgValue = IntegerValue("Green", 255, 0, 255) { wordlColorValue.get() }
     val wordlColorbValue = IntegerValue("Blue", 255, 0, 255) { wordlColorValue.get() }
+    //FogColor
+    val fogColorrValue = IntegerValue("Red", 255, 0, 255) { fogColorValue.get() }
+    val fogColorgValue = IntegerValue("Green", 255, 0, 255) { fogColorValue.get() }
+    val fogColorbValue = IntegerValue("Blue", 255, 0, 255) { fogColorValue.get() }
+    val fogDistance = FloatValue("DistanceFog", 0.7f, 0.1f, 2f) { fogColorValue.get() }
 
     //AntiBlind
     val confusionEffect = BoolValue("Confusion", true) { antiBlindValue.get() }
--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinEntityRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinEntityRenderer.java
@@ -7,6 +7,7 @@ package net.ccbluex.liquidbounce.injection.forge.mixins.render;
 
 import com.google.common.base.Predicates;
 import net.ccbluex.liquidbounce.LiquidBounce;
+import net.ccbluex.liquidbounce.event.EventManager;
 import net.ccbluex.liquidbounce.event.Render3DEvent;
 import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura;
 import net.ccbluex.liquidbounce.features.module.modules.player.Reach;
@@ -15,20 +16,30 @@ import net.ccbluex.liquidbounce.features.module.modules.render.NoHurtCam;
 import net.ccbluex.liquidbounce.features.module.modules.render.TargetMark;
 import net.ccbluex.liquidbounce.features.module.modules.render.Tracers;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.GLAllocation;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.resources.IResource;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItemFrame;
 import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.common.MinecraftForge;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -38,7 +49,10 @@ import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
+import java.io.IOException;
+import java.nio.FloatBuffer;
 import java.util.List;
+import java.util.Set;
 
 @Mixin(EntityRenderer.class)
 public abstract class MixinEntityRenderer {
@@ -53,6 +67,23 @@ public abstract class MixinEntityRenderer {
         this.thirdPersonDistanceTemp = thirdPersonDistanceTemp;
         this.thirdPersonDistance = thirdPersonDistance;
     }
+
+    @Shadow
+    private FloatBuffer fogColorBuffer = GLAllocation.createDirectFloatBuffer(16);
+@Shadow
+    private float farPlaneDistance;
+
+    @Shadow
+    private float fogColorRed;
+    @Shadow
+    private float fogColorGreen;
+    @Shadow
+    private float fogColorBlue;
+
+    @Shadow
+    private float fogColor2;
+    @Shadow
+    private float fogColor1;
 
     @Shadow
     public abstract void loadShader(ResourceLocation resourceLocationIn);
@@ -137,6 +168,10 @@ public abstract class MixinEntityRenderer {
             callbackInfo.cancel();
     }
 
+    /**
+     * @author
+     * @reason
+     */
     @Overwrite
     private void updateLightmap(float f2) {
         Camera camera = LiquidBounce.moduleManager.getModule(Camera.class);
@@ -240,9 +275,302 @@ public abstract class MixinEntityRenderer {
         }
     }
 
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    private void updateFogColor(float p_updateFogColor_1_) {
+        Camera camera = LiquidBounce.moduleManager.getModule(Camera.class);
+        World world = this.mc.theWorld;
+        Entity entity = this.mc.getRenderViewEntity();
+        float f = camera.getState() && camera.getFogColorValue().get() ? camera.getFogDistance().get() * 10.0f : 0.25f + 0.75f * (float)this.mc.gameSettings.renderDistanceChunks / 32.0f;
+        f = 1.0F - (float)Math.pow((double)f, 0.25);
+        Vec3 vec3 = world.getSkyColor(this.mc.getRenderViewEntity(), p_updateFogColor_1_);
+        float f1 = (float)vec3.xCoord;
+        float f2 = (float)vec3.yCoord;
+        float f3 = (float)vec3.zCoord;
+        Vec3 vec31 = world.getFogColor(p_updateFogColor_1_);
+        this.fogColorRed = (float)vec31.xCoord;
+        this.fogColorGreen = (float)vec31.yCoord;
+        this.fogColorBlue = (float)vec31.zCoord;
+        float f12;
+        if (this.mc.gameSettings.renderDistanceChunks >= 4) {
+            double d0 = -1.0;
+            Vec3 vec32 = MathHelper.sin(world.getCelestialAngleRadians(p_updateFogColor_1_)) > 0.0F ? new Vec3(d0, 0.0, 0.0) : new Vec3(1.0, 0.0, 0.0);
+            f12 = (float)entity.getLook(p_updateFogColor_1_).dotProduct(vec32);
+            if (f12 < 0.0F) {
+                f12 = 0.0F;
+            }
+
+            if (f12 > 0.0F) {
+                float[] afloat = world.provider.calcSunriseSunsetColors(world.getCelestialAngle(p_updateFogColor_1_), p_updateFogColor_1_);
+                if (afloat != null) {
+                    f12 *= afloat[3];
+                    this.fogColorRed = this.fogColorRed * (1.0F - f12) + afloat[0] * f12;
+                    this.fogColorGreen = this.fogColorGreen * (1.0F - f12) + afloat[1] * f12;
+                    this.fogColorBlue = this.fogColorBlue * (1.0F - f12) + afloat[2] * f12;
+                }
+            }
+        }
+
+        this.fogColorRed += (f1 - this.fogColorRed) * f;
+        this.fogColorGreen += (f2 - this.fogColorGreen) * f;
+        this.fogColorBlue += (f3 - this.fogColorBlue) * f;
+        float f8 = world.getRainStrength(p_updateFogColor_1_);
+        float f9;
+        float f11;
+        if (f8 > 0.0F) {
+            f9 = 1.0F - f8 * 0.5F;
+            f11 = 1.0F - f8 * 0.4F;
+            this.fogColorRed *= f9;
+            this.fogColorGreen *= f9;
+            this.fogColorBlue *= f11;
+        }
+
+        f9 = world.getThunderStrength(p_updateFogColor_1_);
+        if (f9 > 0.0F) {
+            f11 = 1.0F - f9 * 0.5F;
+            this.fogColorRed *= f11;
+            this.fogColorGreen *= f11;
+            this.fogColorBlue *= f11;
+        }
+
+        Block block = ActiveRenderInfo.getBlockAtEntityViewpoint(this.mc.theWorld, entity, p_updateFogColor_1_);
+        if (this.cloudFog) {
+            Vec3 vec33 = world.getCloudColour(p_updateFogColor_1_);
+            this.fogColorRed = (float)vec33.xCoord;
+            this.fogColorGreen = (float)vec33.yCoord;
+            this.fogColorBlue = (float)vec33.zCoord;
+        } else if (block.getMaterial() == Material.water) {
+            f12 = (float) EnchantmentHelper.getRespiration(entity) * 0.2F;
+            if (entity instanceof EntityLivingBase && ((EntityLivingBase)entity).isPotionActive(Potion.waterBreathing)) {
+                f12 = f12 * 0.3F + 0.6F;
+            }
+
+            this.fogColorRed = 0.02F + f12;
+            this.fogColorGreen = 0.02F + f12;
+            this.fogColorBlue = 0.2F + f12;
+        } else if (block.getMaterial() == Material.lava) {
+            this.fogColorRed = 0.6F;
+            this.fogColorGreen = 0.1F;
+            this.fogColorBlue = 0.0F;
+        }
+
+        f12 = this.fogColor2 + (this.fogColor1 - this.fogColor2) * p_updateFogColor_1_;
+        this.fogColorRed *= f12;
+        this.fogColorGreen *= f12;
+        this.fogColorBlue *= f12;
+        double d1 = (entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * (double)p_updateFogColor_1_) * world.provider.getVoidFogYFactor();
+        if (entity instanceof EntityLivingBase && ((EntityLivingBase)entity).isPotionActive(Potion.blindness)) {
+            int i = ((EntityLivingBase)entity).getActivePotionEffect(Potion.blindness).getDuration();
+            if (i < 20) {
+                d1 *= (double)(1.0F - (float)i / 20.0F);
+            } else {
+                d1 = 0.0;
+            }
+        }
+
+        if (d1 < 1.0) {
+            if (d1 < 0.0) {
+                d1 = 0.0;
+            }
+
+            d1 *= d1;
+            this.fogColorRed = (float)((double)this.fogColorRed * d1);
+            this.fogColorGreen = (float)((double)this.fogColorGreen * d1);
+            this.fogColorBlue = (float)((double)this.fogColorBlue * d1);
+        }
+
+        float f15;
+        if (this.bossColorModifier > 0.0F) {
+            f15 = this.bossColorModifierPrev + (this.bossColorModifier - this.bossColorModifierPrev) * p_updateFogColor_1_;
+            this.fogColorRed = this.fogColorRed * (1.0F - f15) + this.fogColorRed * 0.7F * f15;
+            this.fogColorGreen = this.fogColorGreen * (1.0F - f15) + this.fogColorGreen * 0.6F * f15;
+            this.fogColorBlue = this.fogColorBlue * (1.0F - f15) + this.fogColorBlue * 0.6F * f15;
+        }
+
+        float f6;
+        if (entity instanceof EntityLivingBase && ((EntityLivingBase)entity).isPotionActive(Potion.nightVision)) {
+            f15 = this.getNightVisionBrightness((EntityLivingBase)entity, p_updateFogColor_1_);
+            f6 = 1.0F / this.fogColorRed;
+            if (f6 > 1.0F / this.fogColorGreen) {
+                f6 = 1.0F / this.fogColorGreen;
+            }
+
+            if (f6 > 1.0F / this.fogColorBlue) {
+                f6 = 1.0F / this.fogColorBlue;
+            }
+
+            this.fogColorRed = this.fogColorRed * (1.0F - f15) + this.fogColorRed * f6 * f15;
+            this.fogColorGreen = this.fogColorGreen * (1.0F - f15) + this.fogColorGreen * f6 * f15;
+            this.fogColorBlue = this.fogColorBlue * (1.0F - f15) + this.fogColorBlue * f6 * f15;
+        }
+
+        if (this.mc.gameSettings.anaglyph) {
+            f15 = (this.fogColorRed * 30.0F + this.fogColorGreen * 59.0F + this.fogColorBlue * 11.0F) / 100.0F;
+            f6 = (this.fogColorRed * 30.0F + this.fogColorGreen * 70.0F) / 100.0F;
+            float f7 = (this.fogColorRed * 30.0F + this.fogColorBlue * 70.0F) / 100.0F;
+            this.fogColorRed = f15;
+            this.fogColorGreen = f6;
+            this.fogColorBlue = f7;
+        }
+
+        EntityViewRenderEvent.FogColors event = new EntityViewRenderEvent.FogColors(new EntityRenderer(Minecraft.getMinecraft(), new IResourceManager() {
+            @Override
+            public Set<String> getResourceDomains() {
+                return null;
+            }
+
+            @Override
+            public IResource getResource(ResourceLocation resourceLocation) throws IOException {
+                return null;
+            }
+
+            @Override
+            public List<IResource> getAllResources(ResourceLocation resourceLocation) throws IOException {
+                return null;
+            }
+        }), entity, block, (double)p_updateFogColor_1_, this.fogColorRed, this.fogColorGreen, this.fogColorBlue);
+        MinecraftForge.EVENT_BUS.post(event);
+        this.fogColorRed = event.red;
+        this.fogColorGreen = event.green;
+        this.fogColorBlue = event.blue;
+        GlStateManager.clearColor(this.fogColorRed, this.fogColorGreen, this.fogColorBlue, 0.0F);
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    private void setupFog(int p_setupFog_1_, float p_setupFog_2_) {
+        Camera camera = LiquidBounce.moduleManager.getModule(Camera.class);
+        Entity entity = this.mc.getRenderViewEntity();
+        boolean flag = false;
+        if (entity instanceof EntityPlayer) {
+            flag = ((EntityPlayer)entity).capabilities.isCreativeMode;
+        }
+
+        GL11.glFog(2918, this.setFogColorBuffer(this.fogColorRed, this.fogColorGreen, this.fogColorBlue, 1.0F));
+        GL11.glNormal3f(0.0F, -1.0F, 0.0F);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        Block block = ActiveRenderInfo.getBlockAtEntityViewpoint(this.mc.theWorld, entity, p_setupFog_2_);
+        float hook = ForgeHooksClient.getFogDensity(new EntityRenderer(Minecraft.getMinecraft(), new IResourceManager() {
+            @Override
+            public Set<String> getResourceDomains() {
+                return null;
+            }
+
+            @Override
+            public IResource getResource(ResourceLocation resourceLocation) throws IOException {
+                return null;
+            }
+
+            @Override
+            public List<IResource> getAllResources(ResourceLocation resourceLocation) throws IOException {
+                return null;
+            }
+        }), entity, block, p_setupFog_2_, 0.1F);
+        if (hook >= 0.0F) {
+            GlStateManager.setFogDensity(hook);
+        } else {
+            float f;
+            if (entity instanceof EntityLivingBase && ((EntityLivingBase)entity).isPotionActive(Potion.blindness)) {
+                f = 5.0F;
+                int i = ((EntityLivingBase)entity).getActivePotionEffect(Potion.blindness).getDuration();
+                if (i < 20) {
+                    f = 5.0F + (this.farPlaneDistance - 5.0F) * (1.0F - (float)i / 20.0F);
+                }
+
+                GlStateManager.setFog(9729);
+                if (p_setupFog_1_ == -1) {
+                    GlStateManager.setFogStart(0.0F);
+                    GlStateManager.setFogEnd(f * 0.8F);
+                } else {
+                    GlStateManager.setFogStart(f * 0.25F);
+                    GlStateManager.setFogEnd(f);
+                }
+
+                if (GLContext.getCapabilities().GL_NV_fog_distance) {
+                    GL11.glFogi(34138, 34139);
+                }
+            } else if (this.cloudFog) {
+                GlStateManager.setFog(2048);
+                GlStateManager.setFogDensity(0.1F);
+            } else if (block.getMaterial() == Material.water) {
+                GlStateManager.setFog(2048);
+                if (entity instanceof EntityLivingBase && ((EntityLivingBase)entity).isPotionActive(Potion.waterBreathing)) {
+                    GlStateManager.setFogDensity(0.01F);
+                } else {
+                    GlStateManager.setFogDensity(0.1F - (float)EnchantmentHelper.getRespiration(entity) * 0.03F);
+                }
+            } else if (block.getMaterial() == Material.lava) {
+                GlStateManager.setFog(2048);
+                GlStateManager.setFogDensity(2.0F);
+            } else {
+                float value = camera.getState() && camera.getFogColorValue().get() ? camera.getFogDistance().getValue() * 50.0f : 0.0f;
+                f = this.farPlaneDistance - value;
+                GlStateManager.setFog(9729);
+                if (p_setupFog_1_ == -1) {
+                    GlStateManager.setFogStart(0.0F);
+                    GlStateManager.setFogEnd(f);
+                } else {
+                    GlStateManager.setFogStart(f * 0.75F);
+                    GlStateManager.setFogEnd(f);
+                }
+
+                if (GLContext.getCapabilities().GL_NV_fog_distance) {
+                    GL11.glFogi(34138, 34139);
+                }
+
+                if (this.mc.theWorld.provider.doesXZShowFog((int)entity.posX, (int)entity.posZ)) {
+                    GlStateManager.setFogStart(f * 0.05F);
+                    GlStateManager.setFogEnd(Math.min(f, 192.0F) * 0.5F);
+                }
+
+                ForgeHooksClient.onFogRender(new EntityRenderer(Minecraft.getMinecraft(), new IResourceManager() {
+                    @Override
+                    public Set<String> getResourceDomains() {
+                        return null;
+                    }
+
+                    @Override
+                    public IResource getResource(ResourceLocation resourceLocation) throws IOException {
+                        return null;
+                    }
+
+                    @Override
+                    public List<IResource> getAllResources(ResourceLocation resourceLocation) throws IOException {
+                        return null;
+                    }
+                }), entity, block, p_setupFog_2_, p_setupFog_1_, f);
+            }
+        }
+
+        if (camera.getState() && camera.getFogColorValue().get()) {
+            this.fogColorRed = camera.getFogColorrValue().get() / 255.0f;
+            this.fogColorGreen = camera.getFogColorgValue().get() / 255.0f;
+            this.fogColorBlue = camera.getFogColorbValue().get() / 255.0f;
+        }else{
+            this.fogColorRed = this.fogColorRed;
+            this.fogColorGreen = this.fogColorGreen;
+            this.fogColorBlue = this.fogColorBlue;
+        }
+        GlStateManager.enableColorMaterial();
+        GlStateManager.enableFog();
+        GlStateManager.colorMaterial(1028, 4608);
+    }
+
     private float getNightVisionBrightness(EntityLivingBase p_getNightVisionBrightness_1_, float p_getNightVisionBrightness_2_) {
         int i = p_getNightVisionBrightness_1_.getActivePotionEffect(Potion.nightVision).getDuration();
         return i > 200 ? 1.0F : 0.7F + MathHelper.sin(((float)i - p_getNightVisionBrightness_2_) * 3.1415927F * 0.2F) * 0.3F;
+    }
+    private FloatBuffer setFogColorBuffer(float p_setFogColorBuffer_1_, float p_setFogColorBuffer_2_, float p_setFogColorBuffer_3_, float p_setFogColorBuffer_4_) {
+        this.fogColorBuffer.clear();
+        this.fogColorBuffer.put(p_setFogColorBuffer_1_).put(p_setFogColorBuffer_2_).put(p_setFogColorBuffer_3_).put(p_setFogColorBuffer_4_);
+        this.fogColorBuffer.flip();
+        return this.fogColorBuffer;
     }
 
     @Inject(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Vec3;distanceTo(Lnet/minecraft/util/Vec3;)D"), cancellable = true)
