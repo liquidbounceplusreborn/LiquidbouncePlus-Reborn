cae42ceb1f4e175484800d7ce088f4df41e64e16
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2022-12-25 15:34:13 +0800

    Update LegitSpeed.java

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/LegitSpeed.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/LegitSpeed.java
@@ -11,19 +11,13 @@ import net.minecraft.potion.Potion;
 
 @ModuleInfo(name = "LegitSpeed", description = "Allows you to move faster.", category = ModuleCategory.MOVEMENT)
 public class LegitSpeed extends Module {
-    public final FloatValue Boost = new FloatValue("Boost", 0.03F, 0F, 0.1F);
-    public final FloatValue Boost1 = new FloatValue("Boost1", 0.14F, 0F, 0.2F);
-    public final FloatValue Boost2 = new FloatValue("Boost2", 0.17F, 0F, 0.3F);
+    public final FloatValue Boost1 = new FloatValue("Boost1", 0.03F, 0F, 0.1);
+    public final FloatValue Boost2 = new FloatValue("Boost2", 0.07, 0F, 0.2);
 
     @EventTarget
     public void onJump(JumpEvent event) {
         if (mc.thePlayer != null || MovementUtils.isMoving()) {
             float boost;
-            if (mc.thePlayer.isPotionActive(Potion.moveSpeed) && mc.thePlayer.getActivePotionEffect(Potion.moveSpeed).getAmplifier() != 1) {
-                boost = (Boost.getValue());
-                mc.thePlayer.motionX *= (1.0f + (float)BaseSpeed() * boost);
-                mc.thePlayer.motionZ *= (1.0f + (float)BaseSpeed() * boost);
-            }
             if (mc.thePlayer.isPotionActive(Potion.moveSpeed) && mc.thePlayer.getActivePotionEffect(Potion.moveSpeed).getAmplifier() != 2) {
                 boost = (Boost1.getValue());
                 mc.thePlayer.motionX *= (1.0f + (float)BaseSpeed() * boost);
