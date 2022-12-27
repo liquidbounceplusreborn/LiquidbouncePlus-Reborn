bc742c5bb4722191e18779c8ec970f04d9cbe3fe
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2022-12-27 13:05:11 +0800

    Update Velocity.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
@@ -35,7 +35,7 @@ class Velocity : Module() {
     private val verticalValue = FloatValue("Vertical", 0F, -1F, 1F, "x")
     private val horizontalExplosionValue = FloatValue("HorizontalExplosion", 0F, 0F, 1F, "x")
     private val verticalExplosionValue = FloatValue("VerticalExplosion", 0F, 0F, 1F, "x")
-    private val modeValue = ListValue("Mode", arrayOf("Cancel", "Simple", "Hypixel","AACv4", "AAC4Reduce", "AAC5Reduce", "AAC5.2.0", "AAC", "AACPush", "AACZero","Intave",
+    private val modeValue = ListValue("Mode", arrayOf("Cancel", "Simple","AACv4", "AAC4Reduce", "AAC5Reduce", "AAC5.2.0", "AAC", "AACPush", "AACZero","Intave",
         "Reverse", "SmoothReverse", "Jump", "Glitch", "Phase", "Matrix", "Legit",  "AEMine"), "Cancel") // later
 
     private val aac5KillAuraValue = BoolValue("AAC5.2.0-Attack-Only", true, { modeValue.get().equals("aac5.2.0", true) })
@@ -278,15 +278,6 @@ class Velocity : Module() {
                     packet.motionY = (packet.getMotionY() * vertical).toInt()
                     packet.motionZ = (packet.getMotionZ() * horizontal).toInt()
                 }
-                "Hypixel" -> {
-                    if (packet is S12PacketEntityVelocity) {
-                        val packet = event.packet as S12PacketEntityVelocity
-                        if (packet.entityID == mc.thePlayer.entityId) {
-                            event.cancelEvent()
-                    }
-                }
-            }
-
                 "aac4reduce" -> {
                     velocityInput = true
                     packet.motionX = (packet.getMotionX() * 0.6).toInt()
