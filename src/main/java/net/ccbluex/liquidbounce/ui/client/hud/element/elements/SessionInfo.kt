ef1124c164c1a6174b36a92d61ed4d89810a5c0f
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2023-02-16 18:57:19 +0800

    recode session info(color bug)

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/SessionInfo.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/SessionInfo.kt
@@ -1,64 +1,87 @@
 package net.ccbluex.liquidbounce.ui.client.hud.element.elements
 
-import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
+import net.ccbluex.liquidbounce.features.module.modules.render.ColorMixer
 import net.ccbluex.liquidbounce.ui.client.hud.element.Border
 import net.ccbluex.liquidbounce.ui.client.hud.element.Element
 import net.ccbluex.liquidbounce.ui.client.hud.element.ElementInfo
-import net.ccbluex.liquidbounce.ui.client.hud.element.Side
 import net.ccbluex.liquidbounce.ui.font.Fonts
+import net.ccbluex.liquidbounce.utils.ServerUtils
+import net.ccbluex.liquidbounce.utils.SessionUtils
+import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.FloatValue
-import net.ccbluex.liquidbounce.value.FontValue
 import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.ListValue
+import net.minecraft.entity.player.EntityPlayer
 import java.awt.Color
 
-@ElementInfo(name = "SessionInfo") class SessionInfo(x: Double = 15.0, y: Double = 10.0, scale: Float = 1F, side: Side = Side(Side.Horizontal.LEFT, Side.Vertical.UP)) : Element(x, y, scale, side) {
+@ElementInfo(name = "SessionInfo")
+class SessionInfo(x: Double = 10.0, y: Double = 10.0, scale: Float = 1F) : Element(x, y, scale) {
+    private val colorModeValue = ListValue("Color", arrayOf("Custom","Sky", "CRainbow", "LiquidSlowly", "Fade", "Mixer"), "Custom")
+    private val modeValue = ListValue("Mode", arrayOf("1", "2"), "1")
+    val colorRedValue = IntegerValue("Red", 255,255, 255)
+    val colorGreenValue = IntegerValue("Green", 255, 0, 255)
+    val colorBlueValue = IntegerValue("Blue", 255, 0, 255)
+    private val saturationValue = FloatValue("Saturation", 0.9f, 0f, 1f)
+    private val brightnessValue = FloatValue("Brightness", 1f, 0f, 1f)
+    private val skyDistanceValue = IntegerValue("Sky-Distance", 2, -4, 4)
+    private val cRainbowSecValue = IntegerValue("CRainbow-Seconds", 2, 1, 10)
+    private val cRainbowDistValue = IntegerValue("CRainbow-Distance", 2, 1, 6)
+    private val mixerSecValue = IntegerValue("Mixer-Seconds", 2, 1, 10)
+    private val mixerDistValue = IntegerValue("Mixer-Distance", 2, 0, 10)
+    private val liquidSlowlyDistanceValue = IntegerValue("LiquidSlowly-Distance", 90, 1, 90)
+    private val fadeDistanceValue = IntegerValue("Fade-Distance", 50, 1, 100)
+    
+    val counter = intArrayOf(0)
 
-    private val radiusValue = FloatValue("Radius", 4.25f, 0f, 10f)
-    private val bgredValue = IntegerValue("Bg-R", 255, 0, 255)
-    private val bggreenValue = IntegerValue("Bg-G", 255, 0, 255)
-    private val bgblueValue = IntegerValue("Bg-B", 255, 0, 255)
-    private val bgalphaValue = IntegerValue("Bg-Alpha", 150, 0, 255)
+    val colorMode = colorModeValue.get()
+    var Sky = RenderUtils.SkyRainbow(counter[0] * (skyDistanceValue.get() * 50), saturationValue.get(), brightnessValue.get())
+    var CRainbow = RenderUtils.getRainbowOpaque(cRainbowSecValue.get(), saturationValue.get(), brightnessValue.get(), counter[0] * (50 * cRainbowDistValue.get()))
+    var FadeColor = ColorUtils.fade(Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), 255), fadeDistanceValue.get(), 100).rgb
+    val test = ColorUtils.LiquidSlowly(System.nanoTime(), liquidSlowlyDistanceValue.get(), saturationValue.get(), brightnessValue.get())?.rgb
+    var LiquidSlowly : Int = test!!
+    val mixerColor = ColorMixer.getMixedColor(mixerDistValue.get() * 10, mixerSecValue.get()).rgb
+    val customColor = Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(),255).rgb
 
-    val lineValue = BoolValue("Line", true)
-    private val redValue = IntegerValue("Line-R", 255, 0, 255)
-    private val greenValue = IntegerValue("Line-G", 255, 0, 255)
-    private val blueValue = IntegerValue("Line-B", 255, 0, 255)
-    private val colorRedValue2 = IntegerValue("Line-R2", 0, 0, 255)
-    private val colorGreenValue2 = IntegerValue("Line-G2", 111, 0, 255)
-    private val colorBlueValue2 = IntegerValue("Line-B2", 255, 0, 255)
 
-    val fontValue = FontValue("Font", Fonts.font35)
-
-    override fun drawElement(): Border? {
-        val fontRenderer = fontValue.get()
-
-        val y2 = fontRenderer.FONT_HEIGHT * 3 + 11.0
-        val x2 = 140.0
-
-        var durationInMillis: Long = System.currentTimeMillis() - LiquidBounce.playTimeStart
-        var second = durationInMillis / 1000 % 60
-        var minute = durationInMillis / (1000 * 60) % 60
-        var hour = durationInMillis / (1000 * 60 * 60) % 24
-        var time: String
-        time = String.format("%02dh %02dm %02ds", hour, minute, second)
-
-        RenderUtils.drawRoundedRect(-2f, -2f, x2.toFloat(), y2.toFloat(), radiusValue.get(), Color(bgredValue.get(), bggreenValue.get(), bgblueValue.get(), bgalphaValue.get()).rgb)
-        if(lineValue.get()) {
-            RenderUtils.drawGradientSideways(
-                2.44,
-                fontRenderer.FONT_HEIGHT + 2.5 + 0.0,
-                138.0 + -2.44,
-                fontRenderer.FONT_HEIGHT + 2.5 + 1.16,
-                Color(redValue.get(), greenValue.get(), blueValue.get()).rgb,
-                Color(colorRedValue2.get(), colorGreenValue2.get(), colorBlueValue2.get()).rgb
+    override fun drawElement(): Border {
+        val target: EntityPlayer? = mc.thePlayer
+        val convertedTarget = target!!
+        if (modeValue.get().equals("1")) {
+            RenderUtils.drawCircleRect(0f, 0f, 165f, 63f ,5f ,Color(0, 0, 0, 120).rgb)
+            RenderUtils.drawRect(0f, 12f, 165f, 13f,
+                when {
+                colorMode.equals("Sky", ignoreCase = true) -> Sky
+                colorMode.equals("CRainbow", ignoreCase = true) -> CRainbow
+                colorMode.equals("LiquidSlowly", ignoreCase = true) -> LiquidSlowly
+                colorMode.equals("Fade", ignoreCase = true) -> FadeColor
+                colorMode.equals("Mixer", ignoreCase = true) -> mixerColor
+                else -> customColor
+                }
             )
+            Fonts.fontTahoma.drawString("SessionInfo" , 165 / 2 - Fonts.fontTahoma.getStringWidth("SessionInfo") / 2, 3, Color(0xFFFFFF).rgb)
         }
-        fontRenderer.drawStringWithShadow("Session Info", x2.toFloat() / 4f, 3f, Color.WHITE.rgb)
-        fontRenderer.drawStringWithShadow("Play Time: $time", 2f, fontRenderer.FONT_HEIGHT + 8f, Color.WHITE.rgb)
-        fontRenderer.drawStringWithShadow("Players Killed: " + KillAura.CombatListener.killCounts,2f , fontRenderer.FONT_HEIGHT * 2 + 8f, Color.WHITE.rgb)
-        return Border(-2f, -2f, x2.toFloat(), y2.toFloat())
+        if (modeValue.get().equals("2")) {
+            RenderUtils.drawRect(0f, 0f, 165f, 63f ,Color(0, 0, 0, 120).rgb)
+            RenderUtils.drawRect(0f, 0f, 165f, 1f,                 when {
+                colorMode.equals("Sky", ignoreCase = true) -> Sky
+                colorMode.equals("CRainbow", ignoreCase = true) -> CRainbow
+                colorMode.equals("LiquidSlowly", ignoreCase = true) -> LiquidSlowly
+                colorMode.equals("Fade", ignoreCase = true) -> FadeColor
+                colorMode.equals("Mixer", ignoreCase = true) -> mixerColor
+                else -> customColor
+            })
+            Fonts.fontTahoma.drawString("SessionInfo" , 2, 4, Color(0xFFFFFF).rgb)
+        }
+        Fonts.fontSFUI35.drawString("Playtime"  , 2 , 15 , Color(0xFFFFFF).rgb)
+        Fonts.fontSFUI35.drawString(SessionUtils.getFormatSessionTime() , 165 - Fonts.fontSFUI35.getStringWidth(SessionUtils.getFormatSessionTime()) - 3 , 15 , Color(0xFFFFFF).rgb)
+        Fonts.fontSFUI35.drawString("Kills" , 2 , 27 , Color(0xFFFFFF).rgb)
+        Fonts.fontSFUI35.drawString("${KillAura.CombatListener.killCounts}" , 165 - Fonts.fontSFUI35.getStringWidth("${KillAura.CombatListener.killCounts}") - 3 , 27 , Color(0xFFFFFF).rgb)
+        Fonts.fontSFUI35.drawString("Name", 2 , 39 , Color(0xFFFFFF).rgb)
+        Fonts.fontSFUI35.drawString(convertedTarget.name , 165 - Fonts.fontSFUI35.getStringWidth(convertedTarget.name) - 3 , 39 , Color(0xFFFFFF).rgb)
+        Fonts.fontSFUI35.drawString("Server"  , 2 , 51 , Color(0xFFFFFF).rgb)
+        Fonts.fontSFUI35.drawString(ServerUtils.getRemoteIp()  , 165 - Fonts.fontSFUI35.getStringWidth(ServerUtils.getRemoteIp()) - 3 , 51 , Color(0xFFFFFF).rgb)
+        return Border(0f, 0f, 165f, 63f)
     }
 }
