d30ea41a6892f9adb0e5eecaed06b66e651cc0da
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2023-02-18 18:49:20 +0800

    test watchdog sword noslow(ig watchdog patch eat noslow)

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoSlow.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoSlow.kt
@@ -41,7 +41,7 @@ import net.minecraft.util.EnumFacing
 @ModuleInfo(name = "NoSlow", spacedName = "No Slow", category = ModuleCategory.MOVEMENT, description = "Prevent you from getting slowed down by items (swords, foods, etc.) and liquids.")
 class NoSlow : Module() {
     private val msTimer = MSTimer()
-    private val modeValue = ListValue("PacketMode", arrayOf("Vanilla", "Watchdog", "Blink", "Intave", "NCP", "AAC", "AAC5", "Custom","Test1"), "Vanilla")
+    private val modeValue = ListValue("PacketMode", arrayOf("Vanilla", "Watchdog", "Blink", "Intave", "NCP", "AAC", "AAC5", "Custom"), "Vanilla")
     private val blockForwardMultiplier = FloatValue("BlockForwardMultiplier", 1.0F, 0.2F, 1.0F, "x")
     private val blockStrafeMultiplier = FloatValue("BlockStrafeMultiplier", 1.0F, 0.2F, 1.0F, "x")
     private val consumeForwardMultiplier = FloatValue("ConsumeForwardMultiplier", 1.0F, 0.2F, 1.0F, "x")
@@ -118,7 +118,7 @@ class NoSlow : Module() {
         val packet = event.packet
         val killAura = LiquidBounce.moduleManager[KillAura::class.java]!! as KillAura
 
-        if ((modeValue.get().equals("watchdog", true) || modeValue.get().equals("test2", true)) && packet is S30PacketWindowItems && (mc.thePlayer.isUsingItem || mc.thePlayer.isBlocking)) {
+        if (modeValue.get().equals("watchdog", true) && packet is S30PacketWindowItems && (mc.thePlayer.isUsingItem || mc.thePlayer.isBlocking)) {
             event.cancelEvent()
             if (debugValue.get())
                 ClientUtils.displayChatMessage("detected reset item packet")
@@ -186,47 +186,6 @@ class NoSlow : Module() {
         val killAura = LiquidBounce.moduleManager[KillAura::class.java]!! as KillAura
 
         when (modeValue.get().toLowerCase()) {
-                        "test1"->{
-                if (mc.thePlayer.isUsingItem && mc.thePlayer.heldItem != null && MovementUtils.isMoving()) {
-                    var st = 8
-                    for (i in 0..8) {
-                        if (mc.thePlayer.inventory.getStackInSlot(i) == null) {
-                            st = i
-                        }
-                    }
-                    if (mc.thePlayer.hurtTime == 0  && mc.thePlayer.heldItem != null && mc.thePlayer.heldItem.item is ItemSword) {
-                        if (!released) {
-                            mc.thePlayer.sendQueue.addToSendQueue(
-                                    C07PacketPlayerDigging(
-                                            C07PacketPlayerDigging.Action.RELEASE_USE_ITEM,
-                                            BlockPos.ORIGIN,
-                                            EnumFacing.DOWN
-                                    )
-                            )
-                            released = true
-                        }
-                        return
-                    } else {
-                        released = false
-                    }
-
-                    mc.netHandler.addToSendQueue(C09PacketHeldItemChange(st))
-
-//                for (i in 0..3) {
-                    mc.netHandler.addToSendQueue(
-                            C07PacketPlayerDigging(
-                                    C07PacketPlayerDigging.Action.ABORT_DESTROY_BLOCK,
-                                    BlockPos.ORIGIN,
-                                    EnumFacing.UP
-                            )
-                    )
-//                }
-                    mc.netHandler.addToSendQueue(C09PacketHeldItemChange(mc.thePlayer.inventory.currentItem))
-                }else{
-                    released = false
-                }
-            }
-
             "aac5" -> if (event.eventState == EventState.POST && (mc.thePlayer.isUsingItem || mc.thePlayer.isBlocking || killAura.blockingStatus)) {
                 mc.netHandler.addToSendQueue(C08PacketPlayerBlockPlacement(BlockPos(-1, -1, -1), 255, mc.thePlayer.inventory.getCurrentItem(), 0f, 0f, 0f))
             }
@@ -280,6 +239,14 @@ class NoSlow : Module() {
                     }
                     "ncp" -> sendPacket(event, true, true, false, 0, false)
                     "custom" -> sendPacket(event, customRelease.get(), customPlace.get(), customDelayValue.get() > 0, customDelayValue.get().toLong(), customOnGround.get())
+                    "watchdog"->{
+                        if (!released) {
+                            mc.thePlayer.sendQueue.addToSendQueue(C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN))
+                            released = true
+                        }else{
+                            released = false
+                        }
+                    }
                 }
             }
         }
