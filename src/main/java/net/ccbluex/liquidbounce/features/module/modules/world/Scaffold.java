9ef02cfa0e2f1be32f190c182a399192672f64f8
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2023-03-01 12:55:15 +0800

    Update Scaffold.java

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Scaffold.java
@@ -576,7 +576,7 @@ public class Scaffold extends Module {
         }
         if (sprintModeValue.get().equalsIgnoreCase("Hypixel") && mc.thePlayer.onGround && mc.thePlayer.hurtTime > 0) {
             mc.thePlayer.motionY = 0.0;
-            MovementUtils.strafe(0.47f);
+            MovementUtils.strafe(0.4f);
         }
     }
 
