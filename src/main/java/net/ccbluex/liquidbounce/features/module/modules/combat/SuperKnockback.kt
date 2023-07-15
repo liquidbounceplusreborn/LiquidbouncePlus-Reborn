d624d6786822156d80a79de039117b036b3095bc
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2023-07-15 22:37:40 +0800

    Update SuperKnockback.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/SuperKnockback.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/SuperKnockback.kt
@@ -7,10 +7,13 @@ package net.ccbluex.liquidbounce.features.module.modules.combat
 
 import net.ccbluex.liquidbounce.event.AttackEvent
 import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.utils.Rotation
+import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.utils.timer.MSTimer
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.IntegerValue
@@ -21,47 +24,104 @@ import net.minecraft.network.play.client.C0BPacketEntityAction
 @ModuleInfo(name = "SuperKnockback", spacedName = "Super Knockback", description = "Increases knockback dealt to other entities.", category = ModuleCategory.COMBAT)
 class SuperKnockback : Module() {
     private val hurtTimeValue = IntegerValue("HurtTime", 10, 0, 10)
-    private val modeValue = ListValue("Mode", arrayOf("ExtraPacket", "WTap", "Packet"), "ExtraPacket")
-    //custom useless mode :V
-    private val delay = IntegerValue("Delay", 0, 0, 500, "ms")
+    private val modeValue = ListValue("Mode", arrayOf("Wtap", "Legit", "Silent", "SprintReset", "SneakPacket"), "Silent")
+    private val onlyMoveValue = BoolValue("OnlyMove", true)
+    private val onlyMoveForwardValue = BoolValue("OnlyMoveForward", true){ onlyMoveValue.get() }
+    private val onlyGroundValue = BoolValue("OnlyGround", false)
+    private val delayValue = IntegerValue("Delay", 0, 0, 500)
+
+    private var ticks = 0
 
     val timer = MSTimer()
 
     @EventTarget
-    // i added since LB only have one superKnockback mode.ik there is superkb script that better than this
     fun onAttack(event: AttackEvent) {
         if (event.targetEntity is EntityLivingBase) {
-            if (event.targetEntity.hurtTime > hurtTimeValue.get() || !timer.hasTimePassed(delay.get().toLong()))
+            if (event.targetEntity.hurtTime > hurtTimeValue.get() || !timer.hasTimePassed(delayValue.get().toLong()) ||
+                (!MovementUtils.isMoving() && onlyMoveValue.get()) || (!mc.thePlayer.onGround && onlyGroundValue.get())) {
                 return
-            when (modeValue.get().toLowerCase()) {
-                "extrapacket" -> {
-                    if (mc.thePlayer.isSprinting)
-                        mc.thePlayer.isSprinting = true
-                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING))
+            }
 
-                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING))
-                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING))
-                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING))
-                    mc.thePlayer.serverSprintState = true
+            if (onlyMoveForwardValue.get() && RotationUtils.getRotationDifference(Rotation(movingYaw, mc.thePlayer.rotationPitch), Rotation(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch)) > 35) {
+                return
+            }
+
+            when (modeValue.get().lowercase()) {
+
+                "wtap" ->  ticks = 2
+
+
+                "legit" -> {
+                    ticks = 2
                 }
 
-                "wtap" -> {
-                    if (mc.thePlayer.isSprinting)
-                        mc.thePlayer.isSprinting = false
-                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING))
-                    mc.thePlayer.serverSprintState = true
+                "silent" -> {
+                    ticks = 1
                 }
-                "packet" -> {
-                    if(mc.thePlayer.isSprinting)
-                        mc.thePlayer.isSprinting = true
+
+                "sprintreset" -> {
                     mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING))
+                }
+
+                "sneakpacket" -> {
+                    if (mc.thePlayer.isSprinting) {
+                        mc.thePlayer.isSprinting = true
+                    }
+                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING))
+                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SNEAKING))
                     mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING))
+                    mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SNEAKING))
                     mc.thePlayer.serverSprintState = true
                 }
             }
             timer.reset()
         }
     }
-    override val tag: String?
+
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        if (modeValue.equals("Legit")) {
+            if (ticks == 2) {
+                mc.gameSettings.keyBindForward.pressed = false
+                ticks = 1
+            } else if (ticks == 1) {
+                mc.gameSettings.keyBindForward.pressed = true
+                ticks = 0
+            }
+        }
+        if (modeValue.equals("Wtap")) {
+            if (ticks == 2) {
+                mc.thePlayer.isSprinting = false
+                ticks = 1
+            } else if (ticks == 1) {
+                mc.thePlayer.isSprinting = true
+                ticks = 0
+            }
+        }
+        if (modeValue.equals("Silent")) {
+            if (ticks == 1) {
+                mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING))
+                ticks = 2
+            } else if (ticks == 2) {
+                mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING))
+                ticks = 0
+            }
+        }
+    }
+
+    override val tag: String
         get() = modeValue.get()
+
+    val direction: Double
+        get() {
+            var rotationYaw = mc.thePlayer.rotationYaw
+            if (mc.thePlayer.movementInput.moveForward < 0f) rotationYaw += 180f
+            var forward = 1f
+            if (mc.thePlayer.movementInput.moveForward < 0f) forward = -0.5f else if (mc.thePlayer.movementInput.moveForward > 0f) forward = 0.5f
+            if (mc.thePlayer.movementInput.moveStrafe > 0f) rotationYaw -= 90f * forward
+            if (mc.thePlayer.movementInput.moveStrafe < 0f) rotationYaw += 90f * forward
+            return Math.toRadians(rotationYaw.toDouble())
+        }
+    private val movingYaw: Float
+        get() = (direction * 180f / Math.PI).toFloat()
 }
