444d6bf2f2c85195a43a531cafa5a73b83feb52a
randomguy3725 <110034079+randomguy3725@users.noreply.github.com>
2023-07-23 19:10:56 +0800

    omg

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Fly.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Fly.java
@@ -944,9 +944,12 @@ public class Fly extends Module {
         }
 
         if (modeValue.isMode("GrimTNT")) {
+            if (mc.thePlayer.hurtTime > 0) {
             if(event.getEventState() == EventState.PRE) {
-                if (mc.thePlayer.hurtTime > 0) {
-                    mc.thePlayer.setPositionAndRotation(mc.thePlayer.posX + 1000, mc.thePlayer.posY, mc.thePlayer.posZ + 1000, mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch);
+                    mc.thePlayer.setPositionAndRotation(mc.thePlayer.posX + 1000, mc.thePlayer.posY, mc.thePlayer.posZ, mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch);
+                }
+                if(event.getEventState() == EventState.POST) {
+                    mc.thePlayer.setPositionAndRotation(mc.thePlayer.posX - 1000, mc.thePlayer.posY, mc.thePlayer.posZ, mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch);
                 }
             }
         }
