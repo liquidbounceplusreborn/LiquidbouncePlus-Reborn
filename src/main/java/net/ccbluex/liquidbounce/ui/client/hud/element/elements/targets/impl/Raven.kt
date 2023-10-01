e96a93681cee411cc7784ce85e8479cb465cc3ed
Randumbguy <110034079+randomguy3725@users.noreply.github.com>
2023-10-02 01:20:20 +0800

    Update Raven.kt

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/targets/impl/Raven.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/targets/impl/Raven.kt
@@ -12,6 +12,7 @@ class Raven(inst: Target): TargetStyle("Raven", inst, false) {
     override fun drawTarget(entity: EntityPlayer) {
         val maxHealth = entity.maxHealth
         val health = entity.health
+        val healthString = "${decimalFormat2.format(entity.health)} "
         val targetName = entity.name
         val x = 0.0
         val y = 0.0
@@ -21,7 +22,7 @@ class Raven(inst: Target): TargetStyle("Raven", inst, false) {
 
         Fonts.minecraftFont.drawStringWithShadow("Target: $targetName", (x + 6).toFloat(), (y + 5).toFloat(),Color(255, 255, 255).rgb)
         Fonts.minecraftFont.drawStringWithShadow("Health: ", (x + 6).toFloat(), (y + 9 + Fonts.minecraftFont.FONT_HEIGHT).toFloat(), Color(255, 255, 255).rgb)
-        Fonts.minecraftFont.drawStringWithShadow(health.toString(), (x + 6 + Fonts.minecraftFont.getStringWidth("Health: ")).toFloat(), (y + 9 + Fonts.minecraftFont.FONT_HEIGHT).toFloat(), targetInstance.barColor.rgb)
+        Fonts.minecraftFont.drawStringWithShadow(healthString, (x + 6 + Fonts.minecraftFont.getStringWidth("Health: ")).toFloat(), (y + 9 + Fonts.minecraftFont.FONT_HEIGHT).toFloat(), targetInstance.barColor.rgb)
 
         val winorlose = if(health / maxHealth <= mc.thePlayer.health / mc.thePlayer.maxHealth) "W" else "L"
         Fonts.minecraftFont.drawStringWithShadow(winorlose, (x + 6 + Fonts.minecraftFont.getStringWidth("Target: $targetName ")).toFloat(), (y + 5).toFloat(), (if (winorlose == "W")  Color(0, 255, 0).rgb else Color(139, 0, 0).rgb))
