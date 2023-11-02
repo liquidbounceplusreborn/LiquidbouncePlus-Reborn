b836b6c3d4e966fa8362a465d65334e8536e4db2
4pii4 <138131197+4pii4@users.noreply.github.com>
2023-11-02 13:44:11 +0700

    refactored anti staff and improve accuracy

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AntiStaff.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AntiStaff.kt
@@ -17,9 +17,11 @@ import net.ccbluex.liquidbounce.ui.client.hud.element.elements.Notification
 import net.ccbluex.liquidbounce.ui.client.hud.element.elements.NotifyType
 import net.ccbluex.liquidbounce.utils.ClientUtils
 import net.ccbluex.liquidbounce.utils.EntityUtils
+import net.ccbluex.liquidbounce.utils.ServerUtils
 import net.ccbluex.liquidbounce.utils.misc.HttpUtils
 import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.value.BoolValue
+import net.minecraft.entity.Entity
 import net.minecraft.network.play.server.*
 import kotlin.concurrent.thread
 
@@ -30,13 +32,17 @@ import kotlin.concurrent.thread
     category = ModuleCategory.MISC
 )
 class AntiStaff : Module() {
+    private val notify = BoolValue("Notify", true)
+    private val chat = BoolValue("Chat", true)
     private val leave = BoolValue("Leave", true)
 
     private var staffs = mutableListOf<String>()
     private var staffsInWorld = mutableListOf<String>()
-    private var detected = false
 
     private var staffList: String = "${LiquidBounce.CLIENT_CLOUD}/staffs.txt"
+
+    private val onBMC: Boolean
+        get() = ServerUtils.serverData.serverIP.contains("blocksmc.com")
 
     override fun onInitialize() {
         thread {
@@ -46,89 +52,131 @@ class AntiStaff : Module() {
     }
 
     override fun onEnable() {
-        detected = false
         staffsInWorld.clear()
     }
 
     @EventTarget
     fun onWorld(e: WorldEvent) {
-        detected = false
         staffsInWorld.clear()
     }
 
-    private fun warn(name: String, reason: String) {
+    private fun warn(name: String) {
         if (name in staffsInWorld)
             return
-        chat("[AntiStaff] ${name}: $reason")
-        LiquidBounce.hud.addNotification(Notification("AntiStaff", "${name}: $reason.", NotifyType.ERROR))
+
+        val msg = if (leave.get()) ", leaving" else ""
+        if (chat.get())
+            chat("[AntiStaff] Detected staff: $name$msg")
+        if (notify.get())
+            LiquidBounce.hud.addNotification(Notification("AntiStaff", "Detected staff: $name$msg", NotifyType.ERROR, 4000))
         if (leave.get())
             mc.thePlayer.sendChatMessage("/leave")
+
         staffsInWorld.add(name)
+    }
+    
+    private fun isStaff(entity: Entity): Boolean {
+        return entity.name in staffs || entity.displayName.unformattedText in staffs
     }
 
     @EventTarget
     fun onPacket(event: PacketEvent) {
-        if (mc.theWorld == null || mc.thePlayer == null) return
+        if (mc.theWorld == null || mc.thePlayer == null || !onBMC) return
 
-        val packet = event.packet // smart convert
-        if (packet is S1DPacketEntityEffect) {
-            val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "effect")
+        when (val packet = event.packet) {
+            is S0CPacketSpawnPlayer -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
             }
-        }
-        if (packet is S18PacketEntityTeleport) {
-            val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "teleport")
-            }
-        }
-        if (packet is S20PacketEntityProperties) {
-            val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "properties")
-            }
-        }
-        if (packet is S0BPacketAnimation) {
-            val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "animation")
-            }
-        }
-        if (packet is S14PacketEntity) {
-            val entity = packet.getEntity(mc.theWorld) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "update")
-            }
-        }
-        if (packet is S19PacketEntityStatus) {
-            val entity = packet.getEntity(mc.theWorld) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "status")
 
+            is S1EPacketRemoveEntityEffect -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
             }
-        }
-        if (packet is S19PacketEntityHeadLook) {
-            val entity = packet.getEntity(mc.theWorld) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "head")
+
+            is S01PacketJoinGame -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
             }
-        }
-        if (packet is S49PacketUpdateEntityNBT) {
-            val entity = packet.getEntity(mc.theWorld) ?: return
-            if (staffs.contains(entity.name) || staffs.contains(entity.displayName.unformattedText)) {
-                warn(entity.name, "nbt")
+
+            is S04PacketEntityEquipment -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S1CPacketEntityMetadata -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S1DPacketEntityEffect -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S18PacketEntityTeleport -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S20PacketEntityProperties -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S0BPacketAnimation -> {
+                val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S14PacketEntity -> {
+                val entity = packet.getEntity(mc.theWorld) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S19PacketEntityStatus -> {
+                val entity = packet.getEntity(mc.theWorld) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S19PacketEntityHeadLook -> {
+                val entity = packet.getEntity(mc.theWorld) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
+            }
+
+            is S49PacketUpdateEntityNBT -> {
+                val entity = packet.getEntity(mc.theWorld) ?: return
+                if (isStaff(entity))
+                    warn(entity.name)
             }
         }
     }
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        for (networkPlayerInfo in mc.netHandler.playerInfoMap) {
-            val networkName = ColorUtils.stripColor(EntityUtils.getName(networkPlayerInfo)) ?: continue
-            if (networkName in staffs)
-                warn(networkName, "tab")
+        if (mc.theWorld == null || mc.thePlayer == null || !onBMC) return
 
+        mc.netHandler.playerInfoMap.forEach {
+            val networkName = ColorUtils.stripColor(EntityUtils.getName(it))!!.split(" ")[0]
+            if (networkName in staffs)
+                warn(networkName)
+        }
+
+        mc.theWorld.loadedEntityList.forEach {
+            if (it.name in staffs)
+                warn(it.name)
         }
     }
 }
