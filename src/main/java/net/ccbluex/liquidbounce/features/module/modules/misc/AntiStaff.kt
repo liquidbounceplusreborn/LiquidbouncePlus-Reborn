8d8350fdf150db488c194a9fa90837d5c913611d
wxzex <130822426+wxzex@users.noreply.github.com>
2023-11-12 08:42:52 +0200

    gamster,gommehd,hypixel,mush,bmc support --lists except bmc from brayhax

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AntiStaff.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AntiStaff.kt
@@ -29,10 +29,20 @@ import kotlin.concurrent.thread
 class AntiStaff : Module() {
     private var staffs = mutableListOf<String>()
     private var mushmcstaffs = mutableListOf<String>()
+    private var hypixelstaffs = mutableListOf<String>()
+    private var gommehdstaffs = mutableListOf<String>()
+    private var gamsterstaffs = mutableListOf<String>()
+
+
     private var staffsInWorld = mutableListOf<String>()
 
     private var bmcstaffList: String = "${LiquidBounce.CLIENT_CLOUD}/staffs.txt"
     private var mushstaffList: String = "${LiquidBounce.CLIENT_CLOUD}/mushstaffs.txt"
+    private var hypixelstaffList: String = "${LiquidBounce.CLIENT_CLOUD}/hypixelstaffs.txt"
+    private var gommehdstaffList: String = "${LiquidBounce.CLIENT_CLOUD}/gommehdstaffs.txt"
+    private var gamsterstaffList: String = "${LiquidBounce.CLIENT_CLOUD}/gamsterstaffs.txt"
+
+
 
     private val notify = BoolValue("Notify", true)
     private val chat = BoolValue("Chat", true)
@@ -45,11 +55,23 @@ class AntiStaff : Module() {
         get() = ServerUtils.serverData.serverIP.contains("jogar.mush.com.br") || ServerUtils.serverData.serverIP.contains(
             "mush.com.br"
         )
+    private val onHypixel: Boolean
+        get() = ServerUtils.serverData.serverIP.contains("hypixel.net")
+    private val onGommeHD: Boolean
+        get() = ServerUtils.serverData.serverIP.contains("GoomeHD.net")
+    private val onGamster: Boolean
+        get() = ServerUtils.serverData.serverIP.contains("mc.gamster.org")
+
+
 
     override fun onInitialize() {
         thread {
             staffs.addAll(HttpUtils.get(bmcstaffList).split(","))
             mushmcstaffs.addAll(HttpUtils.get(mushstaffList).split(","))
+            gamsterstaffs.addAll(HttpUtils.get(gamsterstaffList).split(","))
+            hypixelstaffs.addAll(HttpUtils.get(hypixelstaffList).split(","))
+            gommehdstaffs.addAll(HttpUtils.get(gommehdstaffList).split(","))
+
 
             ClientUtils.getLogger().info("[Staff/main] $staffs")
         }
@@ -91,8 +113,15 @@ class AntiStaff : Module() {
             return entity.name in staffs || entity.displayName.unformattedText in staffs
         } else if (onMushMC) {
             return entity.name in mushmcstaffs || entity.displayName.unformattedText in mushmcstaffs
+        } else if (onHypixel) {
+            return entity.name in hypixelstaffs || entity.displayName.unformattedText in hypixelstaffs
+        } else if (onGamster) {
+            return entity.name in gamsterstaffs || entity.displayName.unformattedText in gamsterstaffs
+        } else if (onGommeHD) {
+            return entity.name in gommehdstaffs || entity.displayName.unformattedText in gommehdstaffs
+
         }
- 
+
         // Handle the default case if neither onBMC nor onMushMC is true
          return false
     }
@@ -101,143 +130,87 @@ class AntiStaff : Module() {
     @EventTarget
     fun onPacket(event: PacketEvent) {
         if (mc.theWorld == null || mc.thePlayer == null) return
-        if (onBMC)
+        if (onBMC || onMushMC || onHypixel || onGommeHD || onGamster) {
             when (val packet = event.packet) {
                 is S0CPacketSpawnPlayer -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
-                is S1EPacketRemoveEntityEffect -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S01PacketJoinGame -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S04PacketEntityEquipment -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S1CPacketEntityMetadata -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S1DPacketEntityEffect -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S18PacketEntityTeleport -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S20PacketEntityProperties -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S0BPacketAnimation -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S14PacketEntity -> {
-                    val entity = packet.getEntity(mc.theWorld) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S19PacketEntityStatus -> {
-                    val entity = packet.getEntity(mc.theWorld) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S19PacketEntityHeadLook -> {
-                    val entity = packet.getEntity(mc.theWorld) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-                is S49PacketUpdateEntityNBT -> {
-                    val entity = packet.getEntity(mc.theWorld) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
-            }
 
-        if (onMushMC)
-            when (val packet = event.packet) {
-                is S0CPacketSpawnPlayer -> {
-                    val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
-                    if (isStaff(entity))
-                        warn(entity.name)
-                }
                 is S1EPacketRemoveEntityEffect -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S01PacketJoinGame -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S04PacketEntityEquipment -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S1CPacketEntityMetadata -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S1DPacketEntityEffect -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S18PacketEntityTeleport -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S20PacketEntityProperties -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityId) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S0BPacketAnimation -> {
                     val entity = mc.theWorld.getEntityByID(packet.entityID) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S14PacketEntity -> {
                     val entity = packet.getEntity(mc.theWorld) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S19PacketEntityStatus -> {
                     val entity = packet.getEntity(mc.theWorld) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S19PacketEntityHeadLook -> {
                     val entity = packet.getEntity(mc.theWorld) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
+
                 is S49PacketUpdateEntityNBT -> {
                     val entity = packet.getEntity(mc.theWorld) ?: return
                     if (isStaff(entity))
                         warn(entity.name)
                 }
             }
+        }
     }
 
     @EventTarget
